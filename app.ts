ab61bcfd89fa836ab108b98227bec141c355b4d2
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-06-17 15:29:06 +0900

    Initial commit

--- app.ts
+++ app.ts
@@ -1,20 +1,17 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  //JavaScriptでのエラーの吐き出し方
-  // if (typeof n1 !== `number` || typeof n2 !== `number`) {
-  //   throw new Error(`入力値が正しくありません`);
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+//明示的に型を定義
+const personA: {
+  name: string;
+  age: number;
+} = {
+  name: `yota`,
+  age: 30,
+};
+console.log(personA.name);
 
-let number1: number;
-number1 = `Hello`;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = `Result: `;
+//型推論で定義　★こちらの方がベストプラクティス
+const personB = {
+  name: `yota`,
+  age: 30,
+};
 
-add(number1, number2, printResult, resultPhrase);
+console.log(personB.name);
