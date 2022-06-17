ec8d3566f4c5aad35ba27c9a25cd940c5c8e5e3f
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-06-17 14:36:07 +0900

    JavaScript VS TypeScript

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-// function add(n1, n2) {
-//   return n1 + n2;
-// }
-//
-// const number1 = "5";
-// const number2 = 2.8;
-//
-// const result = add(number1, number2);
-// console.log(result);
-//引数に型を指定
 function add(n1, n2) {
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+        throw new Error("\u5165\u529B\u5024\u304C\u6B63\u3057\u304F\u3042\u308A\u307E\u305B\u3093");
+    }
     return n1 + n2;
 }
-var number1 = "5";
+var number1 = 5;
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,29 +1,13 @@
-// function add(n1, n2) {
-//   return n1 + n2;
-// }
-//
-// const number1 = "5";
-// const number2 = 2.8;
-//
-// const result = add(number1, number2);
-// console.log(result);
-
-
-
-//引数に型を指定するとエラーになる
 function add(n1: number, n2: number) {
+  //JavaScriptでのエラーの吐き出し方
+  // if (typeof n1 !== `number` || typeof n2 !== `number`) {
+  //   throw new Error(`入力値が正しくありません`);
+  // }
   return n1 + n2;
 }
 
-const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
 
 const result = add(number1, number2);
 console.log(result);
-//エラー
-// $ tsc app.ts
-// app.ts:19:20 - error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
-// 
-// 19 const result = add(number1, number2);
-
-
